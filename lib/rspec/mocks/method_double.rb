92c68e94373354755db364c6ab89b3506283b27a
Jon Rowe <hello@jonrowe.co.uk>
2013-08-12 22:05:15 +0100

    refactor simple expectation to allow use as an expectation as well as an allowance

--- lib/rspec/mocks/method_double.rb
+++ lib/rspec/mocks/method_double.rb
@@ -244,19 +244,29 @@ module RSpec
         stub
       end
 
-      # A simple stub can only return a concrete value for a message, and
-      # cannot match on arguments. It is used as an optimization over
-      # `add_stub` where it is known in advance that this is all that will be
-      # required of a stub, such as when passing attributes to the `double`
-      # example method.
+      # A simple method double can only return a concrete value for a message,
+      # and cannot match on arguments. It is used as an optimization over
+      # `add_stub` / `add_expectation` where it is known in advance that this
+      # is all that will be required of a method double, such as when passing
+      # attributes to the `double` example method.
       #
       # @private
       def add_simple_stub(method_name, response)
+        setup_simple_method_double method_name, response, stubs
+      end
+
+      # @private
+      def add_simple_expectation(method_name, response, error_generator)
+        setup_simple_method_double method_name, response, expectations, error_generator
+      end
+
+      # @private
+      def setup_simple_method_double(method_name, response, collection, error_generator = nil)
         define_proxy_method
 
-        stub = SimpleMessageExpectation.new(method_name, response)
-        stubs.unshift stub
-        stub
+        me = SimpleMessageExpectation.new(method_name, response, error_generator)
+        collection.unshift me
+        me
       end
 
       # @private
